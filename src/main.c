ae1deb5c34280f2d254d2be3de99b16a093a3bb5
yealugii <128780523+yealugii@users.noreply.github.com>
2023-12-10 03:30:11 -0800

    Update main.c

--- src/main.c
+++ src/main.c
@@ -13,7 +13,7 @@ int main()
 				.target = LOAD_RPX_TARGET_SD_CARD,
 				.filesize = 0,
 				.fileoffset = 0,
-				.path = "wiiu/apps/soh/soh.rpx",
+				.path = "wiiu/apps/crispy-doom-u/crispy-doom-u.rpx",
 			};
 
 			if(Mocha_LaunchRPX(&info) != MOCHA_RESULT_SUCCESS)
